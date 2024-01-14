e893e47710e2cb46c1c3e6bba9cc9f76feaba3ea
TALGATKK <TALGATKK@GMAIL.COM>
2024-01-15 01:45:42 +0600

    hw20

--- app.js
+++ app.js
@@ -5,30 +5,37 @@ function showTable() {
 }
 
 function createTable(data) {
-  var table = "<table border=1>";
-  table += `<tr>
+  try {
+    var table = "<table border=1>";
+    table += `<tr>
                 <th>ID</th>
                 <th colspan="5">Данные</th>
               </tr>`;
-  table += `<tr>
+    table += `<tr>
                 <th></th>
                 <th>Название</th>
                 <th>Изображение</th>
                 <th>Цена</th>
                 <th>Категория</th>
                 <th>Описание</th>
+                <th>Рейтинг</th>
               </tr>`;
-  var tr = "";
-  for (let i = 0; i < data.length; i++) {
-    tr += "<tr>";
-    tr += `<td>${data[i].id}</td>`;
-    tr += `<td>${data[i].title}</td>`;
-    tr += `<td><image src="${data[i].image}" width="100" height="100"></td>`;
-    tr += `<td>${data[i].price}</td>`;
-    tr += `<td>${data[i].category}</td>`;
-    tr += `<td>${data[i].description}</td>`;
-    tr += `<td>${data[i].rating.rate}</td>`;
-    tr += "</tr>";
+    var tr = "";
+    for (let i = 0; i < data.length; i++) {
+      tr += "<tr>";
+      tr += `<td>${data[i].id}</td>`;
+      tr += `<td>${data[i].title}</td>`;
+      tr += `<td><image src="${data[i].image}" width="100" height="100"></td>`;
+      tr += `<td>${data[i].price}</td>`;
+      tr += `<td>${data[i].category}</td>`;
+      tr += `<td>${data[i].description}</td>`;
+      tr += `<td>${data[i].rating.rate}</td>`;
+      tr += "</tr>";
+    }
+  } catch (err) {
+    document.getElementById("goods").innerHTML = err.message;
+  } finally {
+    document.getElementById("goods").value = "";
   }
   table += tr + "</table>";
   document.getElementById("goods").innerHTML = table;
@@ -41,51 +48,51 @@ function loadGoods() {
     .then((data) => createTableF(data));
 }
 function createTableF(data) {
-  var out = "";
-  var table = "<table border=1>";
-  table += `<tr>
+  try {
+    var out = "";
+    var table = "<table border=1>";
+    table += `<tr>
                 <th>ID</th>
                 <th colspan="5">Данные</th>
               </tr>`;
-  table += `<tr>
+    table += `<tr>
                 <th></th>
                 <th>Название</th>
                 <th>Изображение</th>
                 <th>Цена</th>
                 <th>Категория</th>
                 <th>Описание</th>
+                <th>Рейтинг</th>
               </tr>`;
-  var tr = "";
-  var cat = document.getElementById("categoryid");
-  var min = document.getElementById("minprice");
-  var max = document.getElementById("maxprice");
-  var minprice = Number(min.value);
-  var maxprice = Number(max.value);
+    var tr = "";
+    var cat = document.getElementById("categoryid");
+    var min = document.getElementById("minprice");
+    var max = document.getElementById("maxprice");
+    var minprice = Number(min.value);
+    var maxprice = Number(max.value);
 
-  console.log(cat.value);
-  categoryF = "";
-  console.log(data.length);
-  console.log(minprice);
-  console.log(maxprice);
-
-  for (let i = 0; i < data.length; i++) {
-    if (
-      (data[i].category === cat.value || cat.value === "hide") &&
-      (Number(data[i].price) >= minprice || minprice === 0) &&
-      (Number(data[i].price) <= maxprice || maxprice === 0)
-    ) {
-      tr += "<tr>";
-      tr += `<td>${data[i].id}</td>`;
-      tr += `<td>${data[i].title}</td>`;
-      tr += `<td><image src="${data[i].image}" width="100" height="100"></td>`;
-      tr += `<td>${data[i].price}</td>`;
-      tr += `<td>${data[i].category}</td>`;
-      tr += `<td>${data[i].description}</td>`;
-      tr += `<td>${data[i].rating.rate}</td>`;
+    for (let i = 0; i < data.length; i++) {
+      if (
+        (data[i].category === cat.value || cat.value === "hide") &&
+        (Number(data[i].price) >= minprice || minprice === 0) &&
+        (Number(data[i].price) <= maxprice || maxprice === 0)
+      ) {
+        tr += "<tr>";
+        tr += `<td>${data[i].id}</td>`;
+        tr += `<td>${data[i].title}</td>`;
+        tr += `<td><image src="${data[i].image}" width="100" height="100"></td>`;
+        tr += `<td>${data[i].price}</td>`;
+        tr += `<td>${data[i].category}</td>`;
+        tr += `<td>${data[i].description}</td>`;
+        tr += `<td>${data[i].rating.rate}</td>`;
+      }
     }
+  } catch (err) {
+    document.getElementById("goods").innerHTML = err.message;
+  } finally {
+    document.getElementById("goods").value = "";
   }
 
   table += tr + "</table>";
   document.getElementById("goods").innerHTML = table;
-  // document.body.innerHTML += table;
 }
